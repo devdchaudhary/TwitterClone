d91501be9309548b78de63260dd011cb9f0afa19
devanshuchaudhary <devanshu88@gmail.com>
2023-02-26 21:17:22 +0530

    Initial Commit

---
+++ SlidingTabView/SlidingTabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlidingTabViewApp.swift
+//  SlidingTabView
+//
+//  Created by dddiehard on 26/02/23.
+//
+
+import SwiftUI
+
+@main
+struct SlidingTabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
